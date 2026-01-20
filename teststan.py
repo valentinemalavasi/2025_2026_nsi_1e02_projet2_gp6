602e5b326376c296ffceb85db7bc2aa76f68862e
stanigoat <stan.paturel@gmail.com>
2026-01-20 17:08:00 +0100

    Développement d'une fonction encore plus polyvalente

--- teststan.py
+++ teststan.py
@@ -4,14 +4,13 @@ import numpy as np
 
 df = pd.read_csv("InfoCrimes.csv", sep=";")
 
-def crimes_par_annee(df, annee):
-    crimes = df[df["annee"] == annee]["indicateur"]
-    return crimes.unique().tolist()
 
-crimes_2018 = crimes_par_annee(df, 2018)
-print(crimes_2018)
+df["annee"] = df["annee"].astype(int)
+df["nombre"] = df["nombre"].astype(int)
+df["Code_departement"] = df["Code_departement"].astype(str)
+df["Code_region"] = df["Code_region"].astype(str)
 
-def tableau_crimes_occurrences(df, annee):
+def tableau_crimes_occurrences_par_annee(df, annee):
     filtre = df
         
     filtre = filtre[filtre["annee"] == annee]
@@ -25,4 +24,49 @@ def tableau_crimes_occurrences(df, annee):
         .reset_index(drop=True)
     )
 
-print(tableau_crimes_occurrences(df, 2019))
+def compare_crime_annee(df, type_crime, annee1, annee2):
+    pass
+
+def tableau_crimes_occurrences(
+    df,
+    annee=None,
+    code_departement=None,
+    code_region=None,
+    indicateur=None
+):
+    filtre = df.copy()
+
+    if annee is not None:
+        filtre = filtre[filtre["annee"] == annee]
+
+    if code_departement is not None:
+        filtre = filtre[filtre["Code_departement"] == code_departement]
+
+    if code_region is not None:
+        filtre = filtre[filtre["Code_region"] == code_region]
+
+    if indicateur is not None:
+        filtre = filtre[filtre["indicateur"] == indicateur]
+
+    return (
+        filtre
+        .groupby("indicateur", as_index=False)["nombre"]
+        .sum()
+        .rename(columns={"nombre": "occurrence_totale"})
+        .sort_values(by="occurrence_totale", ascending=False)
+        .reset_index(drop=True)
+    )
+
+
+print(tableau_crimes_occurrences_par_annee(df, 2018))
+print(tableau_crimes_occurrences(df, 2018, "74", None, "Violences sexuelles"))
+
+#Faire un graphique extérieur à l'invite de console
+#Faire en sorte que les fonctions soient plus polyvalentes : une fonction de tri qui peut faire année et département en même temps
+#Commander le logiciel depuis l'interface graphique avec Tkinter
+#Faire toutes les merdes : main menu etc
+
+#Liste de fonctionnalités nécessaires :
+#   -Extraire les données selon un ou plusieurs paramètres ( fonction qui renvoie les crimes commis en telle année, en tel lieu, de telle manières etc)
+#   -Fonctionnalité de compraison de ces statistiques ( crimes de 2018 vs 2019 ( qui peut être combiné avec les options du dessus ) )
+#   -Ergonomie maximale sur l'interface graphique en priorité par rapport à l'esthétisme
