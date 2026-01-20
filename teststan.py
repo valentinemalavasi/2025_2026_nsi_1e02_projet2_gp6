4668e767da70b41d1ff8449c27cffbe19c23d88e
stanigoat <stan.paturel@gmail.com>
2026-01-20 16:12:39 +0100

    Amélioration des fonctions avec le nombre d'itérations

--- teststan.py
+++ teststan.py
@@ -10,3 +10,19 @@ def crimes_par_annee(df, annee):
 
 crimes_2018 = crimes_par_annee(df, 2018)
 print(crimes_2018)
+
+def tableau_crimes_occurrences(df, annee):
+    filtre = df
+        
+    filtre = filtre[filtre["annee"] == annee]
+
+    return (
+        filtre
+        .groupby("indicateur", as_index=False)["nombre"]
+        .sum()
+        .rename(columns={"nombre": "occurrence_totale"})
+        .sort_values(by="occurrence_totale", ascending=False)
+        .reset_index(drop=True)
+    )
+
+print(tableau_crimes_occurrences(df, 2019))
