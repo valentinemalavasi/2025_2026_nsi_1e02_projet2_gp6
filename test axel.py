0dcb82dfeb434ee2651752cf668c0a174e1441f2
Axel <vialaaxel@gmail.com>
2026-01-27 17:04:32 +0100

    create test
    j'ai tester l'interface graphique

---
+++ test axel.py
@@ -0,0 +1,23 @@
+from tkinter import * 
+
+# cree une premiere fenètre 
+
+window = Tk()
+
+# personalisation de cette fenètre
+window.title("stan est tres fort en python")
+window.geometry("1080x720")
+window.minsize (480, 360)
+
+# ajouter le texte 
+
+label_title = Label(window, text="Bienvenue sur cette application", font=("Courrier",40), fg="black")
+label_title.pack(expand=YES)
+
+label_subtitle = Label(window, text="" font=("Courrier",40), fg="black")
+label_subtitle.pack(expand=YES)
+
+
+# afficher
+
+window.mainloop ()
