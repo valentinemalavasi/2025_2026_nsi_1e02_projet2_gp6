4719f2a5e1c61b9c090f07173f4c59d83414b03d
Axel <vialaaxel@gmail.com>
2026-02-03 17:09:20 +0100

    Update test axel.py

--- test axel.py
+++ test axel.py
@@ -5,51 +5,34 @@ from tkinter import *
 window = Tk()
 
 # personalisation de cette fenètre
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-window.title("stan est tres nul en python")
-=======
+
+
 window.title("app")
->>>>>>> Stashed changes
-=======
-window.title("app")
->>>>>>> Stashed changes
-=======
-window.title("app")
->>>>>>> Stashed changes
+
 window.geometry("1080x720")
 window.minsize (480, 360)
+window.config (bg='white')
+
 # crée une boite pour que les elements reste la ou ils doivent etre
 
-frame = Frame(window,bg='#AFAFAF')
+frame = Frame(window,bg='#AFAFAF',bd=1, relief= SUNKEN)
 
 # ajouter le texte 
 
-label_title = Label(frame, text="Bienvenue sur cette application", font=("Courrier",40), fg="black")
+label_title = Label(window, text="Bienvenue ", font=("Courrier",40), fg="black", bg='white')
 label_title.pack()
 
-label_subtitle = Label(frame, text="salut", font=("Courrier",25), fg="black")
+label_subtitle = Label(frame , text="année ", font=("Courrier",25), fg="black", bg='white')
 label_subtitle.pack()
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
+
 # ajouter
 
 frame.pack(expand=YES)
-=======
-# ajouter un bouton 
->>>>>>> Stashed changes
 
-=======
+
 # ajouter un bouton 
 
->>>>>>> Stashed changes
-=======
-# ajouter un bouton 
-
->>>>>>> Stashed changes
 bouton_graph = Button(window , text="Graphique" ,font=("Courrier",40), fg="black" , bg='grey' )
 bouton_graph.pack
 # afficher
